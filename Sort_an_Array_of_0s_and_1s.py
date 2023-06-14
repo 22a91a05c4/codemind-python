2b51ef1c59cc8b6cc1f06bac44942b7124d55fb2
Ganugula Satya Sathvika <119840088+22a91a05c4@users.noreply.github.com>
2023-06-14 16:04:13 +0530

    n=int(input()) ls=sorted(list(map(int,input().split()))) for i in range(len(ls)):     print(ls[i],end=" ")

--- Sort_an_Array_of_0s_and_1s.py
+++ Sort_an_Array_of_0s_and_1s.py
@@ -1,8 +1,4 @@
 n=int(input())
-ls=list(map(int,input().split()))
+ls=sorted(list(map(int,input().split())))
 for i in range(len(ls)):
-    if ls[i]==0:
-        print(ls[i],end=' ')
-for j in range(len(ls)):
-    if ls[j]==1:
-        print(ls[j],end=' ')
+    print(ls[i],end=" ")
