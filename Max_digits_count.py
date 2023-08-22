fcd8a75f17d12c064943e4a14a6da93cbdd15996
Ganugula Satya Sathvika <119840088+22a91a05c4@users.noreply.github.com>
2023-08-22 20:39:28 +0530

    a=int(input()) ls=list(map(str,input().split())) k=[] c=0 for i in range(0,a):     m=len(ls[i])     k.append(m) b=max(k) for i in range(len(k)):     if k[i]==b:         c+=1 print(c)

---
+++ Max_digits_count.py
@@ -0,0 +1,12 @@
+a=int(input())
+ls=list(map(str,input().split()))
+k=[]
+c=0
+for i in range(0,a):
+    m=len(ls[i])
+    k.append(m)
+b=max(k)
+for i in range(len(k)):
+    if k[i]==b:
+        c+=1
+print(c)
