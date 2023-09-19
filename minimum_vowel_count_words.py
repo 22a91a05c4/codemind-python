e74e2bcff7351c0cb5678ad398ffe5f099e80d32
Ganugula Satya Sathvika <119840088+22a91a05c4@users.noreply.github.com>
2023-09-19 18:56:25 +0530

    a=list(map(str,input().split())) l=[] for i in a:     c=0     for j in i:         if j in "aeiou":             c+=1     l.append(c) g=min(l) f=0 for i in range(len(l)):     if l[i]==g:         f+=1 print(f)

---
+++ minimum_vowel_count_words.py
@@ -0,0 +1,14 @@
+a=list(map(str,input().split()))
+l=[]
+for i in a:
+    c=0
+    for j in i:
+        if j in "aeiou":
+            c+=1
+    l.append(c)
+g=min(l)
+f=0
+for i in range(len(l)):
+    if l[i]==g:
+        f+=1
+print(f)
