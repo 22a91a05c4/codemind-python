a2008c118d3cb41df171e0c884657c77ffae1e90
Ganugula Satya Sathvika <119840088+22a91a05c4@users.noreply.github.com>
2023-09-21 16:06:13 +0530

    a=int(input()) l=list(map(int,input().split())) k=[] for i in range(len(l)):     c=0     for j in range(len(l)):         if l[j]==l[i]:             c+=1     if l[i]==c:         k.append(l[i]) g=[] for i in k:     if i not in g:         g.append(i) f=len(g) if f==0:     print("-1") else:     v=sum(g)     d=v/f     print("%.2f"%(d))

---
+++ Average_of_super_elements.py
@@ -0,0 +1,21 @@
+a=int(input())
+l=list(map(int,input().split()))
+k=[]
+for i in range(len(l)):
+    c=0
+    for j in range(len(l)):
+        if l[j]==l[i]:
+            c+=1
+    if l[i]==c:
+        k.append(l[i])
+g=[]
+for i in k:
+    if i not in g:
+        g.append(i)
+f=len(g)
+if f==0:
+    print("-1")
+else:
+    v=sum(g)
+    d=v/f
+    print("%.2f"%(d))
