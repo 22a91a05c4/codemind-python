e2dfb5a546725949625d5f24bb80050300a56a3b
Ganugula Satya Sathvika <119840088+22a91a05c4@users.noreply.github.com>
2023-09-21 16:09:32 +0530

    a=int(input()) l=list(map(int,input().split())) k=[] for i in range(len(l)):     c=0     for j in range(len(l)):         if l[j]==l[i]:             c+=1     if l[i]==c:         k.append(l[i]) g=[] for i in k:     if i not in g:         g.append(i) f=len(g) print(f)

---
+++ value_same_as_count.py
@@ -0,0 +1,16 @@
+a=int(input())
+l=list(map(int,input().split()))
+k=[]
+for i in range(len(l)):
+    c=0
+    for j in range(len(l)):
+        if l[j]==l[i]:
+            c+=1
+    if l[i]==c:
+        k.append(l[i])
+g=[]
+for i in k:
+    if i not in g:
+        g.append(i)
+f=len(g)
+print(f)
