ae8a653866f52b507819f879a49a8d301edc68e5
Ganugula Satya Sathvika <119840088+22a91a05c4@users.noreply.github.com>
2023-09-21 16:11:32 +0530

    a=int(input()) l=list(map(int,input().split())) w=int(input()) k=[] for i in range(len(l)):     c=0     for j in range(len(l)):         if l[j]==l[i]:             c+=1     if c==w:         k.append(l[i]) g=[] for i in k:     if i not in g:         g.append(i) f=len(g) if f==0:     print("-1") else:     print(*g)

---
+++ N_times_repeated_elements.py
@@ -0,0 +1,20 @@
+a=int(input())
+l=list(map(int,input().split()))
+w=int(input())
+k=[]
+for i in range(len(l)):
+    c=0
+    for j in range(len(l)):
+        if l[j]==l[i]:
+            c+=1
+    if c==w:
+        k.append(l[i])
+g=[]
+for i in k:
+    if i not in g:
+        g.append(i)
+f=len(g)
+if f==0:
+    print("-1")
+else:
+    print(*g)
